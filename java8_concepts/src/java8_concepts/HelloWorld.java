0506b2fc80814c344e3b515242649757b2ccfff3
Arun Ayyanar <arunctech89@gmail.com>
2023-12-15 08:06:00 -0500

    java refresh started

---
+++ java8_concepts/src/java8_concepts/HelloWorld.java
@@ -0,0 +1,16 @@
+package java8_concepts;
+import java.util.function.Supplier;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		
+		Supplier<String> s = () -> "Hello World!!!";
+		
+		System.out.println(s.get());
+		
+		
+	}
+
+}
---
+++ java_core_concepts/src/java_core_concepts/HelloWorld.java
@@ -0,0 +1,10 @@
+package java_core_concepts;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello world!!!");
+	}
+
+}
